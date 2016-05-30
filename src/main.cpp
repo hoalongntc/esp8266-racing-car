2bb00e39a2ad325a655f1cae009a11a27cad7569
Tung.Nguyen <developer.tungnguyen@gmail.com>
2016-05-30 09:09:48 +0700

    v1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include "Arduino.h"
-#include "Servo.h"
-#include "ESP8266WiFi.h"
+#include <FS.h>
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+
+// Config portal
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+#include <Servo.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 #define TCP_SERVER_PORT 23
@@ -20,121 +30,110 @@
 #define MOTOR_R_BACKWARD_PIN D8
 #define MOTOR_MAXIMUM_SPEED 1023
 
-#define WIFI_STA_MAX_TRY 10000
-// #define CONFIG_WIFI_STA_SSID_KEY "wifi_sta_ssid"
-// #define CONFIG_WIFI_STA_PASS_KEY "wifi_sta_password"
-// #define CONFIG_WIFI_AP_SSID_KEY "wifi_ap_ssid"
-// #define CONFIG_WIFI_AP_PASS_KEY "wifi_ap_password"
-#define WIFI_STA_SSID "DOU_Networks (SCS)"
-#define WIFI_STA_PASS "DOU12345"
-#define WIFI_AP_SSID_DEFAULT "Hoalong-Racing-Car"
-#define WIFI_AP_PASS_DEFAULT "12345678!@#"
+#define WIFI_AP_SSID_DEFAULT "Hoalong-Esp-Config"
+#define WIFI_AP_PASS_DEFAULT "nothing123"
+
+#define MQTT_SERVER "broker.mqtt-dashboard.com"
+#define MQTT_PORT 1883
+#define MQTT_CLIENT_ID "hoalong/racing-car/esp8266"
+#define MQTT_PUBLISH_CHANNEL "hoalong/racing-car/esp8266/ip"
 
 ////////////////////////////////////////////////////////////////////////////////
-// config.json file
+// pins configiguration
+void configPins() {
+  pinMode(MOTOR_L_SPEED_PIN, OUTPUT);
+  analogWrite(MOTOR_L_SPEED_PIN, 0);
 
-// JsonObject& readConfigFileOrCreateInitConfigFileIfNeeded() {
-//   StaticJsonBuffer<200> jsonBuffer;
-//
-//   File configFile;
-//   if (SPIFFS.exists("/config.json")) {
-//     Serial.println("Config file is already existed!");
-//
-//     configFile = SPIFFS.open("/config.json", "r");
-//     size_t size = configFile.size();
-//     if (size > 1024) {
-//       // Recreate new init config file
-//       Serial.println("Config file size is too large");
-//     } else {
-//       // Allocate a buffer to store contents of the file.
-//       std::unique_ptr<char[]> buf(new char[size]);
-//       configFile.readBytes(buf.get(), size);
-//       JsonObject& config = jsonBuffer.parseObject(buf.get());
-//       if (!config.success()) {
-//         // Recreate new init config file
-//         Serial.println("Failed to parse config file");
-//       } else {
-//         return config;
-//       }
-//     }
-//   }
-//
-//   configFile = SPIFFS.open("/config.json", "w");
-//   if (!configFile) {
-//     Serial.println("Failed to open config file for writing");
-//     return jsonBuffer.createObject();
-//   }
-//
-//   JsonObject& initConfig = jsonBuffer.createObject();
-//   initConfig[CONFIG_WIFI_AP_SSID_KEY] = CONFIG_WIFI_AP_SSID_DEFAULT;
-//   initConfig[CONFIG_WIFI_AP_PASS_KEY] = CONFIG_WIFI_AP_PASS_DEFAULT;
-//
-//   initConfig.printTo(configFile);
-//   return initConfig;
-// }
+  pinMode(MOTOR_L_FORWARD_PIN, OUTPUT);
+  digitalWrite(MOTOR_L_FORWARD_PIN, LOW);
+
+  pinMode(MOTOR_L_BACKWARD_PIN, OUTPUT);
+  digitalWrite(MOTOR_L_BACKWARD_PIN, LOW);
+
+  pinMode(MOTOR_R_SPEED_PIN, OUTPUT);
+  analogWrite(MOTOR_R_SPEED_PIN, 0);
+
+  pinMode(MOTOR_R_FORWARD_PIN, OUTPUT);
+  digitalWrite(MOTOR_R_FORWARD_PIN, LOW);
+
+  pinMode(MOTOR_R_BACKWARD_PIN, OUTPUT);
+  digitalWrite(MOTOR_R_BACKWARD_PIN, LOW);
+
+  // initialize led
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // servo instance
 Servo servo;
+void configServo() {
+  servo.attach(SERVO_PIN);
+  servo.write(90);
+  delay(15);
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-// wifi config
+// wifi manager
+WiFiManager wifiManager;
+void configWiFi() {
+  //wifiManager.setSTAStaticIPConfig(IPAddress(10,10,10,10), IPAddress(10,10,10,1), IPAddress(255,255,255,0));
+  wifiManager.setMinimumSignalQuality(20);
+  wifiManager.setTimeout(300);
 
-void WiFiEvent(WiFiEvent_t event) {
-    switch(event) {
-        case WIFI_EVENT_STAMODE_GOT_IP:
-            Serial.println("WiFi connected");
-            Serial.println("IP address: ");
-            Serial.println(WiFi.localIP());
-            digitalWrite(LED_PIN, HIGH);
-            break;
-        case WIFI_EVENT_STAMODE_DISCONNECTED:
-            Serial.println("WiFi lost connection");
-            digitalWrite(LED_PIN, LOW);
-            break;
-    }
-}
-
-bool setupWiFiStationMode(const char* ssid, const char* password) {
-  WiFi.begin(ssid, password);
-  WiFi.onEvent(WiFiEvent);
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  int waited = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-
-    if(waited > WIFI_STA_MAX_TRY) {
-      return false;
-    }
-
-    delay(500);
-    waited += 500;
+  if (!wifiManager.autoConnect(WIFI_AP_SSID_DEFAULT, WIFI_AP_PASS_DEFAULT)) {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again
+    ESP.reset();
+    delay(5000);
   }
 
-  return true;
+  Serial.println("WiFi connected.");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
 }
 
-bool setupWiFiConfigMode(const char* ssid, const char* password) {
-  WiFi.softAP(ssid, password);
+////////////////////////////////////////////////////////////////////////////////
+// pubsub mqtt
+WiFiClient espClient;
+PubSubClient pubsubClient(espClient);
 
-  IPAddress myIP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(myIP);
+boolean retryPublishIp() {
+  if (pubsubClient.connect(MQTT_CLIENT_ID)) {
+      char localIp[20];
+      WiFi.localIP().toString().toCharArray(localIp, 20);
+      pubsubClient.publish(MQTT_PUBLISH_CHANNEL, localIp);
+  }
+  return pubsubClient.connected();
+}
 
-  return true;
+void configPubSub() {
+  pubsubClient.setServer(MQTT_SERVER, MQTT_PORT);
+
+  Serial.print("Attempting MQTT connection...");
+  if (retryPublishIp()) {
+    Serial.println("connected");
+  } else {
+    Serial.print("failed, rc = ");
+    Serial.print(pubsubClient.state());
+    Serial.println(" try again in 3 seconds");
+    // Wait 3 seconds before retrying
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // wifi tcp server
 WiFiServer tcp_server(TCP_SERVER_PORT);
+void configTCPServer() {
+  tcp_server.begin();
+  Serial.println("TCP Server started");
+}
 
+////////////////////////////////////////////////////////////////////////////////
+// command control
 // Accept from -90 to 90
 void onServoEvent(String command) {
-  Serial.println("Servo command " + command);
-
   int command_pos = command.toInt();
   int servo_pos = SERVO_DEFAULT_POS + command_pos;
 
@@ -149,8 +148,6 @@ void onServoEvent(String command) {
 
 // Accept from -255 to 255
 void onMotorEvent(String command) {
-  Serial.println("Motor command " + command);
-
   int direction = servo.read();
   int d_speed_l = direction < 90 ? direction - 90 : 0;
   int d_speed_r = direction > 90 ? 90 - direction : 0;
@@ -208,44 +205,24 @@ void onRequest(String req) {
 // setup call
 void setup()
 {
-  // initialiaze motor
-  pinMode(MOTOR_L_SPEED_PIN, OUTPUT);
-  analogWrite(MOTOR_L_SPEED_PIN, 0);
-
-  pinMode(MOTOR_L_FORWARD_PIN, OUTPUT);
-  digitalWrite(MOTOR_L_FORWARD_PIN, LOW);
-
-  pinMode(MOTOR_L_BACKWARD_PIN, OUTPUT);
-  digitalWrite(MOTOR_L_BACKWARD_PIN, LOW);
-
-  pinMode(MOTOR_R_SPEED_PIN, OUTPUT);
-  analogWrite(MOTOR_R_SPEED_PIN, 0);
-
-  pinMode(MOTOR_R_FORWARD_PIN, OUTPUT);
-  digitalWrite(MOTOR_R_FORWARD_PIN, LOW);
-
-  pinMode(MOTOR_R_BACKWARD_PIN, OUTPUT);
-  digitalWrite(MOTOR_R_BACKWARD_PIN, LOW);
-
-  // initialize led
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
-
   // initialize serial
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  // initialiaze motor
+  configPins();
 
   // initialize servo
-  servo.attach(SERVO_PIN);
-  servo.write(90);
-  delay(15);
+  configServo();
 
-  // setup wifi
-  setupWiFiStationMode(WIFI_STA_SSID, WIFI_STA_PASS);
-  delay(2000);
+  // initialize wifi
+  configWiFi();
+
+  // initialize pubsub
+  configPubSub();
 
   // Start the server
-  tcp_server.begin();
-  Serial.println("TCP Server started");
+  configTCPServer();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -255,38 +232,39 @@ void loop()
   // Check if a client has connected
   WiFiClient client = tcp_server.available();
   if (!client) {
+    retryPublishIp();
+    delay(1000);
     return;
   }
 
   // Wait until the client sends some data
   int waited = 0;
   Serial.println("Client connected");
+  digitalWrite(LED_PIN, HIGH);
   while(client.connected()){
-    if(waited > TCP_SERVER_TIMEOUT) {
-      break; // Client idle
-    }
+    if(waited > TCP_SERVER_TIMEOUT) { break; /* Client idle */ }
 
     // Wait until client say somethings
     while(!client.available()) {
       delay(1);
 
       waited++;
-      if(waited > TCP_SERVER_TIMEOUT) {
-        break; // Client idle
-      }
+      if(waited > TCP_SERVER_TIMEOUT) { break; /* Client idle */ }
     }
 
     if(client.available()) {
       waited = 0;
 
       // Read the first line of the request
+      digitalWrite(LED_PIN, LOW);
       String req = client.readStringUntil('\r');
       onRequest(req);
+      digitalWrite(LED_PIN, HIGH);
     }
   }
 
-  // Send the response to the client
   client.flush();
   delay(1);
-  Serial.println("Client disonnected");
+  Serial.println("Client disconnected");
+  digitalWrite(LED_PIN, LOW);
 }
